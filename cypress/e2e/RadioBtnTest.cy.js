20fc2c3e0c98461e49d3af8eb9047e1f3d6a9e65
Nikhil <nikhilcogni@gmail.com>
2024-01-25 23:05:57 +0000

    Corrected test case description

--- cypress/e2e/RadioBtnTest.cy.js
+++ cypress/e2e/RadioBtnTest.cy.js
@@ -2,7 +2,7 @@
 
 describe("Checkbox demo", ()=>{
 
-    it("Click on checkbox", ()=>{
+    it("Click on Raio button", ()=>{
 
         cy.visit("http://www.webdriveruniversity.com/");
         cy.get("#dropdown-checkboxes-radiobuttons").invoke('removeAttr','target').click();
@@ -12,7 +12,7 @@ describe("Checkbox demo", ()=>{
         cy.get("input[value='blue']").check().should('be.checked');
     });
 
-    it("Click on first checkbox", ()=>{
+    it("Click on first radio button", ()=>{
 
         cy.visit("http://www.webdriveruniversity.com/");
         cy.get("#dropdown-checkboxes-radiobuttons").invoke('removeAttr','target').click();
@@ -21,4 +21,14 @@ describe("Checkbox demo", ()=>{
         cy.get("input[type='radio']").first().check().should('be.checked');
         cy.get("input[type='radio']").last().check().should('be.checked');
     });
+
+    it("Validate whether radio button is disabled", ()=>{
+
+        cy.visit("http://www.webdriveruniversity.com/");
+        cy.get("#dropdown-checkboxes-radiobuttons").invoke('removeAttr','target').click();
+
+        //select radio button
+        cy.get("input[value='cabbage']").should('be.disabled')
+    });
+
 });    
